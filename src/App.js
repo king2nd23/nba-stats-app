d1c913692779930c0d61adb2dc89255db058e7a5
Mark King <king2nd23@gmail.com>
2023-09-19 15:25:11 -0400

    fixing build errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import { useState, useEffect } from "react";
 import './index.css';
-import Search from "./components/Search";
 import StatCardDisplay from "./components/StatCardDisplay";
-import axios from 'axios';
 
 function App () {
     return (
